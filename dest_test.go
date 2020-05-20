29424c4432b3a921b8fbbf94e94a39b160c8876d
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-05-21 00:54:26 +0900

    more test coverage

--- dest_test.go
+++ dest_test.go
@@ -1,12 +1,15 @@
 package zipx
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestDir_CreateDir_Mode(t *testing.T) {
@@ -86,5 +89,90 @@ func TestDir_CreateFile_Mode(t *testing.T) {
 				t.Fatalf("file mode mismatch: want=0%o got=0%o", tc.mode, m)
 			}
 		}
+	}
+}
+
+func TestDir_NoUTF8(t *testing.T) {
+	dir, err := ioutil.TempDir("", "Dir_NoUTF8*")
+	if err != nil {
+		t.Fatalf("failed to TempDir: %s", err)
+	}
+	t.Cleanup(func() {
+		os.RemoveAll(dir)
+	})
+	d := Dir(dir)
+
+	err = d.CreateDir("foo", DirInfo{NonUTF8: true, Mode: 0777})
+	if err != nil {
+		t.Fatal(err)
+	}
+	path := filepath.Join(dir, "foo")
+	fi, err := os.Stat(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !fi.Mode().IsDir() {
+		t.Fatalf("%s is not directory", path)
+	}
+
+	w, err := d.CreateFile("bar", FileInfo{Mode: 0666, NonUTF8: true})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if wc, ok := w.(io.WriteCloser); ok {
+		wc.Close()
+	}
+	path = filepath.Join(dir, "bar")
+	fi, err = os.Stat(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !fi.Mode().IsRegular() {
+		t.Fatalf("%s is not regular file", path)
+	}
+}
+
+func TestDiscard(t *testing.T) {
+	err := Discard.CreateDir("foo", DirInfo{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	w, err := Discard.CreateFile("bar", FileInfo{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if w != ioutil.Discard {
+		t.Fatal("Discard.CreateFile returns not ioutil.Discard")
+	}
+}
+
+func TestDir_FileWrite(t *testing.T) {
+	dir, err := ioutil.TempDir("", "Dir_FileWrite*")
+	if err != nil {
+		t.Fatalf("failed to TempDir: %s", err)
+	}
+	t.Cleanup(func() {
+		os.RemoveAll(dir)
+	})
+	d := Dir(dir)
+
+	const exp = "The quick brown fox jumps over the lazy dog"
+
+	w, err := d.CreateFile("foo.txt", FileInfo{Mode: 0666})
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Fprint(w, exp)
+	if wc, ok := w.(io.WriteCloser); ok {
+		wc.Close()
+	}
+
+	b, err := ioutil.ReadFile(filepath.Join(dir, "foo.txt"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	act := string(b)
+	if d := cmp.Diff(exp, act); d != "" {
+		t.Fatal("content mismatch: -want +got", exp, act)
 	}
 }
--- zipx_test.go
+++ zipx_test.go
@@ -4,13 +4,28 @@ import (
 	"archive/zip"
 	"bytes"
 	"context"
+	"errors"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
+
+func TestZipX_Concurrency(t *testing.T) {
+	x := New()
+	for _, n := range []int{0, 1, 2, 3, 10, 100, 1000} {
+		act := x.WithConcurrency(n).Concurrency()
+		if act != n {
+			t.Fatalf("concurrency mismatch: want=%d got=%d", n, act)
+		}
+	}
+}
 
 type dummyDest struct {
 	Names []string
@@ -53,10 +68,6 @@ func TestExtractWithMode(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		err = zw.Flush()
-		if err != nil {
-			t.Fatal(err)
-		}
 	}
 	err := zw.Close()
 	if err != nil {
@@ -96,5 +107,144 @@ func TestExtractWithMode(t *testing.T) {
 	}
 	if d := cmp.Diff(exp, act, cmpopts.IgnoreFields(FileInfo{}, "Modified")); d != "" {
 		t.Fatalf("unexpected extract: -want +got\n%s", d)
+	}
+}
+
+func writeTestZipFile(t *testing.T, name string) {
+	f, err := os.Create(name)
+	if err != nil {
+		t.Fatalf("failed to os.Create(%s): %s", name, err)
+	}
+	defer f.Close()
+
+	zw := zip.NewWriter(f)
+	defer zw.Close()
+
+	zf, err := zw.Create("foo.txt")
+	if err != nil {
+		t.Fatalf("failed to Create(foo.txt): %s", err)
+	}
+	fmt.Fprint(zf, "foo")
+
+	zf, err = zw.Create("bar.txt")
+	if err != nil {
+		t.Fatalf("failed to Create(bar.txt): %s", err)
+	}
+	fmt.Fprint(zf, "bar")
+}
+
+func TestExtractFile(t *testing.T) {
+	dir, err := ioutil.TempDir("", "ExtractFile*")
+	if err != nil {
+		t.Fatalf("failed to TempDir: %s", err)
+	}
+	t.Cleanup(func() {
+		os.RemoveAll(dir)
+	})
+
+	name := filepath.Join(dir, "test.zip")
+	writeTestZipFile(t, name)
+
+	outdir := filepath.Join(dir, "outdir")
+	var lastProgress Progress
+	err = New().WithConcurrency(1).
+		WithMonitor(nil).
+		WithMonitor(MonitorFunc(func(p Progress) {
+			lastProgress = p
+		})).
+		ExtractFile(context.Background(), name, Dir(outdir))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if d := cmp.Diff(Progress{NumDone: 2, NumTotal: 2}, lastProgress); d != "" {
+		t.Fatalf("last progress mismatch: -want +got\n%s", d)
+	}
+
+	// FIXME: check content of outdir
+}
+
+func TestExtract_CreateFile_failed(t *testing.T) {
+	dir, err := ioutil.TempDir("", "Extract_DestFailure*")
+	if err != nil {
+		t.Fatalf("failed to TempDir: %s", err)
+	}
+	t.Cleanup(func() {
+		os.RemoveAll(dir)
+	})
+
+	name := filepath.Join(dir, "test.zip")
+	writeTestZipFile(t, name)
+
+	errExp := &errDest{}
+	err = New().WithConcurrency(1).ExtractFile(context.Background(), name, errExp)
+	if err == nil {
+		t.Fatal("unexpected success, must be failed")
+	}
+	if !errors.Is(err, errExp) {
+		t.Fatalf("unexpected error: %s", err)
+	}
+}
+
+func TestExtract_CreateDir_failed(t *testing.T) {
+	bb := &bytes.Buffer{}
+	zw := zip.NewWriter(bb)
+	h := &zip.FileHeader{Name: "foo", Method: zip.Deflate}
+	h.SetMode(os.FileMode(0755) | os.ModeDir)
+	_, err := zw.CreateHeader(h)
+	if err != nil {
+		t.Fatal(err)
+	}
+	zw.Close()
+	zr, err := zip.NewReader(bytes.NewReader(bb.Bytes()), int64(bb.Len()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	errExp := &errDest{}
+	err = New().WithConcurrency(1).Extract(context.Background(), *zr, errExp)
+	if err == nil {
+		t.Fatal("unexpected success, must be failed")
+	}
+	if !errors.Is(err, errExp) {
+		t.Fatalf("unexpected error: %s", err)
+	}
+}
+
+type errDest struct {
+}
+
+func (e *errDest) Error() string {
+	return "error destination"
+}
+
+func (e *errDest) CreateDir(string, DirInfo) error {
+	return e
+}
+
+func (e *errDest) CreateFile(string, FileInfo) (io.Writer, error) {
+	return nil, e
+}
+
+func TestExtract_unsupported_mode(t *testing.T) {
+	bb := &bytes.Buffer{}
+	zw := zip.NewWriter(bb)
+	h := &zip.FileHeader{Name: "foo", Method: zip.Deflate}
+	h.SetMode(os.FileMode(0666) | os.ModeSymlink)
+	_, err := zw.CreateHeader(h)
+	if err != nil {
+		t.Fatal(err)
+	}
+	zw.Close()
+	zr, err := zip.NewReader(bytes.NewReader(bb.Bytes()), int64(bb.Len()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = New().WithConcurrency(1).Extract(context.Background(), *zr, Discard)
+	if err == nil {
+		t.Fatal("unexpected success, must be failed")
+	}
+	if s := err.Error(); !strings.HasPrefix(s, "unsupported file mode ") {
+		t.Fatalf("unexpected error: %s", err)
 	}
 }
